a0813c5f5aad408ba5648f0108b35568a97765d1
CodeSandbox <support@codesandbox.io>
2024-02-05 18:58:44 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import AuthInputs from './components/AuthInputs.jsx';
-import Header from './components/Header.jsx';
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
+import { useState } from "react";
 
 export default function App() {
+  const [styleButton, setStyleButton] = useState("white");
+  function switchStyle() {
+    if (styleButton === "white") {
+      setStyleButton("red");
+    } else {
+      setStyleButton("white");
+    }
+  }
   return (
     <>
       <Header />
       <main>
         <AuthInputs />
       </main>
+
+      <div>
+        <p style={{ color: styleButton }}>Style me!</p>
+        <button onClick={switchStyle}>Toggle style</button>
+      </div>
     </>
   );
 }
